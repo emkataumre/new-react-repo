ddf4ba0e36164bb092f8aa43803e9aa7d06d0c55
emkataumre <emo.vladinov@gmail.com>
2022-11-19 17:28:28 +0100

    Comment about Avatar component
    Added a comment under the <Avatar/> component explaining its origin and purpose.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -10,6 +10,7 @@ import { Avatar } from '@mui/material'
                     src = 'none'
                     />
                     <h3>emkataumre</h3>
+                    {/* Avatar component imported from material UI (A react component library). Later I will pass it the username as from the props in <Post/> (rendered in App.js), as well as imageUrl and caption */}
                 </div>
                 <img src={require('../img/post.jpg')}></img>
                 <h3>Username: Caption</h3>
