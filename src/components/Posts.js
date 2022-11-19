f01870cb34b9aa08d9fff8745d50a1091d4f8bd6
emkataumre <emo.vladinov@gmail.com>
2022-11-19 17:15:59 +0100

    Post prototype
    Added a hardcoded post prototype in posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Post() {
-    return(
-        <div>
-            <h3>Post render test</h3>
-        </div>
-    )
-}
 
+        function Post() {
+            return (
+             <div className='post'>
+                <h3>Username</h3>
+                <img src={require('../img/post.jpg')}></img>
+                <h3>Username: Caption</h3>
+            </div>
+            )
+          }
 export {Post};
