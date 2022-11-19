39f97251781d6c7780ec18d0535a727382719006
emkataumre <emo.vladinov@gmail.com>
2022-11-19 17:06:51 +0100

    Post render test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './css/index.css';
 import {Header} from './components/Header';
-
+import {Post} from './components/Posts';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Post/>  
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Post() {
+    return(
+        <div>
+            <h3>Post render test</h3>
+        </div>
+    )
+}
+
+export {Post};
