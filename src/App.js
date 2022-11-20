2a3de3b0bf66f5a712f888e8d1484555fcc392cb
emkataumre <emo.vladinov@gmail.com>
2022-11-20 14:48:12 +0100

    Efficiency update
    Added doc.id do the newly created object in the docs.map function. The id is referenced in the posts.map function and used as a key. That way, when the database updates, only the data that has changed will be loaded or updated  in the app, as opposed to updating/loading everything again.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import {Header} from './components/Header';
 import {Post} from './components/Posts';
 import {db} from './firebase';
 function App() {
-
-  const [posts, setPosts] = useState([]);
-
   useEffect(() => {
   db.collection('posts') //'posts' here refers to my collection on firebase that is called 'posts'
   .onSnapshot(snapshot => {
-    setPosts(snapshot.docs.map(doc => doc.data()));
+    setPosts(snapshot.docs.map(doc => ({
+      id: doc.id,
+      post: doc.data()})));
   }) //onSnapshot will fire every time a change happens in the 'posts' collection
   
   }, []); //Currently runs once on refresh
+
+const [posts, setPosts] = useState([]);
 
   return (
     <div className="App">
@@ -23,8 +24,8 @@ function App() {
         // This piece of code will look at the array of currently 2 objects and loop through it.
         // On the first loop it will render the values (previous props in <Post/>) of the first object and on the second loop
         // it will render the values of the second object and so on and so forth depending on how many objects are there. 
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        posts.map(({id, post}) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
         ))
       }
       </div>
