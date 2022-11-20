debb5f18bf2b21940a1f88ca599ea2ecd3b37beb
emkataumre <emo.vladinov@gmail.com>
2022-11-20 14:06:08 +0100

    Added map() to update <Post/>; Added array of objects in state hook
    The props from <Post/> have been moved into an array of two objects, wrapped in a State hook (used to track changes). Then a posts.map function has been added that loops through the array of objects when it changes and renders the values as props. That way the app does that same thing as it did in the previous version, but more efficiently and with less code.
    Some comments have also been added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React, { useEffect, useState } from "react";
 import './css/index.css';
 import {Header} from './components/Header';
 import {Post} from './components/Posts';
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+    username: "emkataumre",
+    caption:"new post",
+    imageUrl: "https://assets1.ignimgs.com/2018/06/21/hollowknight-1280-1529623462572_160w.jpg?width=1280"
+    },
+
+    {
+    username: "rogachevumre",
+    caption: "picture",
+    imageUrl: "https://d1fdloi71mui9q.cloudfront.net/6C9NH1a2Q42IPPPd7iSm_0UmAJ3pr4APt97Gg"
+    }
+  ]);
   return (
     <div className="App">
       <Header/>
       <div id='app-container'>
-      <Post username="emkataumre" caption="new post" imageUrl="https://assets1.ignimgs.com/2018/06/21/hollowknight-1280-1529623462572_160w.jpg?width=1280"/>  
-      <Post username="rogachevumre" caption="picture" imageUrl="https://d1fdloi71mui9q.cloudfront.net/6C9NH1a2Q42IPPPd7iSm_0UmAJ3pr4APt97Gg"/>  
-      {/* The props above will be referenced in Post() in Posts.js and later be hooked to the database, instead of being hardcoded */}
+      {
+        // This piece of code will look at the array of currently 2 objects and loop through it.
+        // On the first loop it will render the values (previous props in <Post/>) of the first object and on the second loop
+        // it will render the values of the second object and so on and so forth depending on how many objects are there. 
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
       </div>
     </div>
   );
