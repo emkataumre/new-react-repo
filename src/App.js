cd76edc1df10f29d493103887ba2bccbf5509651
emkataumre <emo.vladinov@gmail.com>
2022-11-20 13:43:41 +0100

    Added props to <Post/>
    Added the username, caption and imageUrl props to <Post/> in App.js and passed them to the Post() function in Post.js. On the next step, the props in App.js should be hooked to the firebase database, instead of being hardcoded.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <Header/>
       <div id='app-container'>
-      <Post/>  
+      <Post username="emkataumre" caption="new post" imageUrl="https://assets1.ignimgs.com/2018/06/21/hollowknight-1280-1529623462572_160w.jpg?width=1280"/>  
+      <Post username="rogachevumre" caption="picture" imageUrl="https://d1fdloi71mui9q.cloudfront.net/6C9NH1a2Q42IPPPd7iSm_0UmAJ3pr4APt97Gg"/>  
+      {/* The props above will be referenced in Post() in Posts.js and later be hooked to the database, instead of being hardcoded */}
       </div>
     </div>
   );
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Avatar } from '@mui/material'
 
-        function Post() {
+        function Post({username, caption, imageUrl}) {
+            //Props above
             return (
              <div className='post'>
                 <div>
                     <Avatar 
-                    alt= 'emkataumre'
+                    alt= {username}
                     src = 'none'
                     />
-                    <h3>emkataumre</h3>
-                    {/* Avatar component imported from material UI (A react component library). Later I will pass it the username as from the props in <Post/> (rendered in App.js), as well as imageUrl and caption */}
+                    <h3>{username}</h3>
                 </div>
-                <img src={require('../img/post.jpg')}></img>
-                <h3>Username: Caption</h3>
+                <img src={imageUrl}></img>
+                <h3>{username}: {caption}</h3>
             </div>
             )
           }
 export {Post};
+
